5ae3b616f20d2222fa99fab77fd3e6fb5e8e620a
AlecVangilbergen <alec.vangilbergen@student.ucll.be>
2022-11-30 18:47:59 +0100

    Test

--- src/main/java/be/cosci/catalog/domain/Product.java
+++ src/main/java/be/cosci/catalog/domain/Product.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-/*This is a test */
 @Entity
 public class Product {
 
